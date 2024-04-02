cce687ba5cc69146c9f5250db3880a026664583e
Git290324 <165396053+Git290324@users.noreply.github.com>
2024-04-02 05:31:06 +0000

    Update setup_passubuntu_mysql.sh

--- setup_passubuntu_mysql.sh
+++ setup_passubuntu_mysql.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Read the temporary password from MySQL error log
+# Đọc mật khẩu tạm thời từ log lỗi MySQL
 TEMP_PASSWORD=$(sudo grep 'temporary password' /var/log/mysql/error.log | awk '{print $NF}')
 
-# Change MySQL root password
-NEW_PASSWORD=$(openssl rand -base64 12)
+# Đặt mật khẩu mới cho người dùng root
+NEW_PASSWORD="PGaming@1107A"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEW_PASSWORD'; FLUSH PRIVILEGES;" | sudo mysql -u root -p"$TEMP_PASSWORD"
 
-# Run mysql_secure_installation
+# Chạy mysql_secure_installation
 echo -e "\n\n\n\nY\n$NEW_PASSWORD\n$NEW_PASSWORD\nY\nY\nY\nY" | sudo mysql_secure_installation
 
-# Access MySQL with new password
+# Truy cập MySQL với mật khẩu mới
 echo "CREATE DATABASE server1;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$NEW_PASSWORD';
 FLUSH PRIVILEGES;
