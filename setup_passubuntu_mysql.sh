b0ed602be1d6f0e57b435e22dcfe8cdd0713fa41
Git290324 <165396053+Git290324@users.noreply.github.com>
2024-04-02 05:38:51 +0000

    Update setup_passubuntu_mysql.sh

--- setup_passubuntu_mysql.sh
+++ setup_passubuntu_mysql.sh
@@ -8,11 +8,10 @@ NEW_PASSWORD="PGaming@1107A"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEW_PASSWORD'; FLUSH PRIVILEGES;" | sudo mysql -u root -p"$TEMP_PASSWORD"
 
 # Chạy mysql_secure_installation
-echo -e "\n\n\n\nY\n$NEW_PASSWORD\n$NEW_PASSWORD\nY\nY\nY\nY" | sudo mysql_secure_installation
+echo -e "\n\n\n\nY\n\n\nY\nY\nY\nY" | sudo mysql_secure_installation
 
-# Xóa cơ sở dữ liệu "server1" nếu tồn tại và tạo lại
-echo "DROP DATABASE IF EXISTS server1;
-CREATE DATABASE server1;
+# Truy cập MySQL với mật khẩu mới
+echo "CREATE DATABASE server1;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$NEW_PASSWORD';
 FLUSH PRIVILEGES;
 exit" | sudo mysql -u root -p"$NEW_PASSWORD"
